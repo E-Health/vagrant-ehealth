e2f10024c08ff2b9a5a4d7be9fc5562b1397d416
Bellraj Eapen <github@gulfdoctor.net>
2015-08-30 10:52:49 -0400

    minor changes

--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -21,4 +21,19 @@ class mysql {
     command => "mysqladmin -uroot -p$password create oscar_mcmaster",
     command => "mysql -uroot -p$password oscar_12_1 < /home/vagrant/OscarON12_1.sql",
   }
+  exec { "Create OSCAR Database":
+    subscribe => [ Package["mysql-server"], Package["mysql-client"], Package["libmysqlclient-dev"] ],
+    refreshonly => true,
+    unless => "mysqladmin -uroot -p$password status",
+    path => "/bin:/usr/bin",
+    command => "mysqladmin -uroot -p$password create oscar_mcmaster",
+  }
+  exec { "Write OSCAR Data":
+    subscribe => [ Package["mysql-server"], Package["mysql-client"], Package["libmysqlclient-dev"] ],
+    refreshonly => true,
+    unless => "mysqladmin -uroot -p$password status",
+    path => "/bin:/usr/bin",
+    command => "mysql -uroot -p$password oscar_12_1 < /home/vagrant/OscarON12_1.sql",
+  }
+
 }
